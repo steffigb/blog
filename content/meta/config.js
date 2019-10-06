15278bf04b45664268d07bc262eb064583309c1f
Steffi Große-Bölting <sgrosseboelting@gmail.com>
2019-10-06 16:27:16 +0200

    first commit

---
+++ content/meta/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: "neue_pfade", // <title>
+  shortSiteTitle: "neue_pfade", // <title> ending for posts and pages
+  siteDescription: "neue_pfade",
+  siteUrl: "https://www.attejuvonen.fi",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "SteffiGB",
+  //authorTwitterAccount: "",
+  //authorGithub: "https://github.com/baobabkoodaa",
+  //authorStackoverflow: "https://stackexchange.com/users/5678032/atte-juvonen?tab=accounts",
+  //authorLinkedin: "https://linkedin.com/in/atte-juvonen-86b886113",
+  //authorCodeforces: "https://codeforces.com/profile/baobab",
+  //authorYoutube: "https://www.youtube.com/channel/UCfHqfHoSYL0V_fXA_JQkeLw?view_as=subscriber",
+
+  /* header text */
+  headerTitle: "neue_pfade",
+  headerSubTitle: "",
+
+  /* manifest.json */
+  manifestName: "neue_pfade",
+  manifestShortName: "neuePfade", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+};
